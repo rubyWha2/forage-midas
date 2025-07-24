4774b32b7c8923f7a6581748ae55d29ae8e2f850
xxxru <r.m.wharton2@newcastle.ac.uk>
2025-07-24 18:01:53 +0100

    Kafka Consumer Class completed

---
+++ src/main/java/com/jpmc/midascore/component/KafkaConsumer.java
@@ -0,0 +1,15 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "${general.kafka-topic}")
+    public void consume(Transaction transaction){
+        System.out.println("Recieved Transaction:" + transaction);
+
+    }
+}
--- src/test/java/com/jpmc/midascore/TaskTwoTests.java
+++ src/test/java/com/jpmc/midascore/TaskTwoTests.java
@@ -11,7 +11,8 @@ import org.springframework.test.annotation.DirtiesContext;
 @SpringBootTest
 @DirtiesContext
 @EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
-class TaskTwoTests {
+class
+TaskTwoTests {
     static final Logger logger = LoggerFactory.getLogger(TaskTwoTests.class);
 
     @Autowired
